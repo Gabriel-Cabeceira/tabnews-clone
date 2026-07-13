6d0c3861f60565e1a5b6ec311751896b1921c6c6
Gabriel Cabeceira <gabriel.goncalves@ascbrazil.com.br>
2026-07-13 17:58:33 -0300

    finish status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,16 +1,37 @@
 import database from "infra/database.js";
 
 async function status (request, response) {
-    const result = await database.query('SELECT 1 + 1;');
-    // console.log('result: ', result);
-    response.status(200).json(
-        {
-            mensagem: "Ménsâgem cõm àcéntuáção",
-            emojis: "😬🤐",
-            emoji_hexadecimal_1: "##1f604##",
-            emoji_hexadecimal_2: "##1f602##"
+
+    const updatedAt = new Date().toISOString();
+    const databaseVersion = await database.query('SHOW server_version;');
+    const databaseVersionValue = databaseVersion.rows[0].server_version;
+
+    const dataBaseMaxConnections = await database.query('SHOW max_connections;');
+    const maxConectionsValue = parseInt(dataBaseMaxConnections.rows[0].max_connections);
+
+    const databaseName = process.env.POSTGRES_DB;
+    const openedConnections = await database.query({
+      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+      values: [databaseName]
+    });
+    const openedConnectionsValue = openedConnections.rows[0].count;
+
+    response.status(200).json({
+      updated_at: updatedAt,
+      dependencies: {
+        database: {
+          // status: 'teste',
+          max_connections: maxConectionsValue,
+          opened_connections: openedConnectionsValue,
+          // latency: {
+          //   first_query: 1,
+          //   second_query: 2,
+          //   third_query: 3
+          // },
+          version: databaseVersionValue
         }
-    )
+      }
+    });
 }
 
 export default status;
