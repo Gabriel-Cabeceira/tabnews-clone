220dd0d2bc64a39b4f36e1527baf9959561fcbfd
Gabriel Cabeceira <gabriel.goncalves@ascbrazil.com.br>
2026-08-02 13:17:49 -0300

    Adds  endpoint

---
+++ tests/integration/api/v1/migrations/post.test.js
@@ -0,0 +1,62 @@
+import database from "infra/database.js";
+import fs from 'fs/promises';
+
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
+  await database.query('drop schema public cascade; create schema public');
+}
+
+test("POST to /api/v1/migrations should return 200", async () => {
+    const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
+      method: 'POST'
+    });
+
+    expect(response1.status).toBe(201);
+
+    const responseBody1 = await response1.json();
+
+    expect(Array.isArray(responseBody1)).toBe(true);
+    expect(responseBody1.length).toBeGreaterThan(0);
+
+
+    const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
+      method: 'POST'
+    });
+
+    expect(response2.status).toBe(200);
+
+    const responseBody2 = await response2.json();
+
+    expect(Array.isArray(responseBody2)).toBe(true);
+    expect(responseBody2.length).toBe(0);
+
+});
+
+
+
+
+// minha forma de implementar - paralelo ao aprensentado no curso
+// test("POST to /api/v1/migrations should return 200", async () => {
+//     const response = await fetch("http://localhost:3000/api/v1/migrations", {
+//       method: 'POST'
+//     });
+
+//     expect(response.status).toBe(200);
+
+//     const responseBody = await response.json();
+
+//     expect(Array.isArray(responseBody)).toBe(true);
+
+
+//     const migrations = await database.query('SELECT * from pgmigrations;');
+
+//     console.log('migrations: ', migrations)
+
+//     expect(migrations.rows.length).toBeGreaterThan(0);
+
+//     const qtdMigrations = await fs.readdir('infra/migrations');
+//     expect(migrations.rows.length).toBe(qtdMigrations.length)
+
+// });
+
